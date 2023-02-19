661d032457ee4d9b828a122ee36cbcfabdd5b12c
biomlds <krivushi@ualberta.ca>
2023-02-19 21:16:36 +0000

    update

--- week4/make_gcp_blocks.py
+++ week4/make_gcp_blocks.py
@@ -8,7 +8,8 @@ from prefect_gcp.cloud_storage import GcsBucket
 
 credentials_block = GcpCredentials(
     # service_account_info={}  # enter your credentials info or use the file method.
-    service_account_file="../../hip-watch-375918-fb1062049386.json"
+    # service_account_file="../../hip-watch-375918-fb1062049386.json"
+    service_account_file="/workspaces/hip-watch-375918-fb1062049386.json"
 )
 credentials_block.save("zoom-gcp-creds", overwrite=True)
 
@@ -17,4 +18,10 @@ bucket_block = GcsBucket(
     bucket="dez-ny-taxi",  # insert your  GCS bucket name
 )
 
+bucket_block = GcsBucket(
+    gcp_credentials=GcpCredentials.load("zoom-gcp-creds"),
+    bucket="dez-ny-taxi-w4",  # insert your  GCS bucket name
+)
+
 bucket_block.save("dez-ny-taxi", overwrite=True)
+bucket_block.save("dez-ny-taxi-w4", overwrite=True)
